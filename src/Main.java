9a4736d6e83a62ac282c860adcf7ad6927ada9df
Lesya <lesik_il_89@mail.ru>
2023-02-26 21:59:40 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 6330;
-        int rublesOneMile = 20;
-        int bonusMile = ticket / rublesOneMile;
-        System.out.println("При покупке билета Вам будет начислено " + bonusMile + " бесплатных миль.");
+        int ticket = 6330;          // стоимость билета
+        int rublesOneMile = 20;     // кол-во рублей для одной бонусной мили
+        int bonusMile = ticket / rublesOneMile;  // кол-во бонусных миль за покупку билета
+        System.out.println("При покупке билета Вам будет начислено " + bonusMile + " бонусных миль.");
     }
 }
